105a4950f0b199001bb7b84fa31513769fc93b6c
Chandra Riyadi <chandra.riyadi@raharja.info>
2022-01-28 18:24:29 +0700

    PUSH

--- pages/register.js
+++ pages/register.js
@@ -2,36 +2,45 @@ import Image from "next/image";
 import Link from "next/link";
 import MainLayout from "../components/layouts/MainLayout";
 import heroRegister from "../public/heroRegister.svg";
-import { useState } from "react";
-import { useRouter } from "next/router";
-import axios from "axios";
+// import { useState } from "react";
+// import { useRouter } from "next/router";
+// import { ErrorMessage } from "@hookform/error-message"
+import { useForm } from "react-hook-form"
+// import axios from "axios";
 
 export default function register() {
-  const router = useRouter();
+  // const router = useRouter();
+
+  const {register, handleSubmit, formState:{errors}, reset} = useForm();  
+
+  const onSubmitForm = (values) => {
+    console.log(values);
+  }
 
   //  State
-  const [form, setForm] = useState({
-    nik: "",
-    nama_lengkap: "",
-    email: "",
-    password: "",
-    alamat: "",
-    jenis_kelamin: "",
-    no_hp: "",
-  });
+  // const [form, setForm] = useState({
+  //   nik: "",
+  //   nama_lengkap: "",
+  //   email: "",
+  //   password: "",
+  //   alamat: "",
+  //   jenis_kelamin: "",
+  //   no_hp: "",
+  // });
 
   // method
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post("http://localhost:8080/api/user/add", form);
-      if (response.data.status === 200) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const response = await axios.post("http://localhost:8080/api/user/add", form);
+  //     console.log(response);
+  //     if (response.data.status === 200) {
+  //       router.push("/");
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <>
@@ -44,148 +53,168 @@ export default function register() {
 
             {/* Form */}
             <div className="w-full mx-auto px-6 py-4 rounded-md bg-gray-50/80">
-              <div>
-                <h2 className="text-center text-sm font-bold tracking-wide text-gray-800">
-                  Selamat Bergabung di
-                  <span className="text-lg font-extrabold tracking-wider text-fuchsia-600"> POJOK UMKM. </span>
-                </h2>
-              </div>
-
-              <form className="2lg:flex 2lg:gap-x-8" onSubmit={handleSubmit}>
-                <div className="w-full py-6">
-                  <div className="block text-sm">
-                    <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>NIK</label>
-                    <input
-                      onChange={(e) => {
-                        setForm({
-                          ...form,
-                          nik: e.target.value,
-                        });
-                      }}
-                      value={form.nik}
-                      type="text"
-                      className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
-                      placeholder="Nomor NIK"
-                    />
-                  </div>
-                  <div className="block text-sm mt-5">
-                    <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Nama Lengkap</label>
-                    <input
-                      onChange={(e) => {
-                        setForm({
-                          ...form,
-                          nama_lengkap: e.target.value,
-                        });
-                      }}
-                      value={form.nama_lengkap}
-                      type="text"
-                      className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
-                      placeholder="Nama Lengkap"
-                    />
-                  </div>
-                  <div className="block text-sm mt-5">
-                    <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>No HP</label>
-                    <input
-                      onChange={(e) => {
-                        setForm({
-                          ...form,
-                          no_hp: e.target.value,
-                        });
-                      }}
-                      value={form.no_hp}
-                      type="text"
-                      className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
-                      placeholder="08xxxxxxxxxx"
-                    />
-                  </div>
-                  <div className="block text-sm mt-5">
-                    <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Alamat Usaha</label>
-                    <textarea
-                      onChange={(e) => {
-                        setForm({
-                          ...form,
-                          alamat: e.target.value,
-                        });
-                      }}
-                      value={form.alamat}
-                      className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
-                      placeholder="Jl. Raya Mauk No.89, Sepatan, Tangerang, Banten 15520, Indonesia"
-                    ></textarea>
-                  </div>
+              <form onSubmit={handleSubmit(onSubmitForm)}>
+                <div>
+                  <h2 className="text-center text-sm font-bold tracking-wide text-gray-800">
+                    Selamat Bergabung di
+                    <span className="text-lg font-extrabold tracking-wider text-fuchsia-600"> POJOK UMKM. </span>
+                  </h2>
                 </div>
 
-                <div className="w-full 2lg:py-6">
-                  <div className="block text-sm">
-                    <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Email</label>
-                    <input
-                      onChange={(e) => {
-                        setForm({
-                          ...form,
-                          email: e.target.value,
-                        });
-                      }}
-                      value={form.email}
-                      type="email"
-                      className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
-                      placeholder="pojokumkm@contoh.com"
-                    />
+                <div className="2lg:flex 2lg:gap-x-8">
+                  <div className="w-full py-6">
+                    <div className="block text-sm">
+                      <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>NIK</label>
+                      <input {...register('nik', {
+                        required:{
+                          value:true,
+                          message:'Masukkan NIK!'
+                        },
+                        pattern:{
+                          value:/^-?[0-9]\d*\.?\d*$/,
+                          message:'Tidak boleh huruf, Masukkan angka!'
+                        },
+                        minLength:{
+                          value:16,
+                          message:'NIK anda kurang lengkap!'
+                        },
+                        maxLength:{
+                          value:16,
+                          message:'Maksimal 16 digit!'
+                        }
+                      })} 
+                        type="text"
+                        className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
+                        placeholder="Nomor NIK"/>
+                        {errors.nik && <p className="px-2 pt-1 text-xs font-medium text-red-500">{errors.nik.message}</p>}
+                    </div>
+                    <div className="block text-sm mt-5">
+                      <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Nama Lengkap</label>
+                      <input {...register('nama_lengkap', {
+                        required:{
+                        value:true,
+                        message:'Masukkan nama lengkap!'
+                        },
+                        minLength:{
+                          value:3,
+                          message:'Nama anda tidak lengkap!'
+                        },
+                        maxLength:{
+                          value:30,
+                          message:'Maksimal 30 huruf!'
+                        }
+                      })}
+                        type="text"
+                        className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
+                        placeholder="Nama Lengkap"/>
+                        {errors.nama_lengkap && <p className="px-2 pt-1 text-xs font-medium text-red-500">{errors.nama_lengkap.message}</p>}
+                    </div>
+                    <div className="block text-sm mt-5">
+                      <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>No HP</label>
+                      <input {...register('no_hp', {
+                        required:{
+                          value:true,
+                          message:'Masukkan nomor HP!'
+                        },
+                        pattern:{
+                          value:/^-?[0-9]\d*\.?\d*$/,
+                          message:'Tidak boleh huruf, Masukkan angka!'
+                        },
+                        minLength:{
+                          value:10,
+                          message:'Nomor HP anda kurang lengkap!'
+                        },
+                        maxLength:{
+                          value:15,
+                          message:'Maksimal 15 digit!'
+                        }
+                      })}
+                        type="text"
+                        className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
+                        placeholder="08xxxxxxxxxx"/>
+                        {errors.no_hp && <p className="px-2 pt-1 text-xs font-medium text-red-500">{errors.no_hp.message}</p>}
+                    </div>
                   </div>
-                  <div className="block text-sm mt-5">
-                    <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Kata Sandi</label>
-                    <input
-                      onChange={(e) => {
-                        setForm({
-                          ...form,
-                          password: e.target.value,
-                        });
-                      }}
-                      value={form.password}
-                      type="password"
-                      className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
-                      placeholder="Kata Sandi"
-                    />
-                  </div>
-                  <div className="block text-sm mt-5">
-                    <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Verifikasi Kata Sandi</label>
-                    <input
-                      type="password"
-                      className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
-                      placeholder="Verifikasi Kata Sandi"
-                    />
-                  </div>
-                  <div className="block text-sm mt-5">
-                    <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Jenis Kelamin</label>
-                    <select
-                      onChange={(e) => {
-                        setForm({
-                          ...form,
-                          jenis_kelamin: e.target.value,
-                        });
-                      }}
-                      value={form.jenis_kelamin}
-                      className="form-select mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
-                    >
-                      <option>Pilih Jenis Kelamin</option>
-                      <option>Laki-laki</option>
-                      <option>Perempuan</option>
-                    </select>
+
+                  <div className="w-full 2lg:py-6">
+                    <div className="block text-sm">
+                      <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Email</label>
+                      <input {...register('email', {
+                        required:{
+                          value:true,
+                          message:'Masukkan email!'
+                        },
+                        pattern:{
+                          value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                          message:'Email tidak Valid!'
+                        }
+                      })}
+                        type="email"
+                        className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
+                        placeholder="pojokumkm@contoh.com"/>
+                        {errors.email && <p className="px-2 pt-1 text-xs font-medium text-red-500">{errors.email.message}</p>}
+                    </div>
+                    <div className="block text-sm mt-5">
+                      <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Kata Sandi</label>
+                      <input {...register('password', {
+                        required:{
+                          value:true,
+                          message:'Masukkan kata sandi!'
+                        },
+                        minLength:{
+                          value:8,
+                          message:'Kata sandi anda kurang spesifik!'
+                        }
+                      })}
+                        type="password"
+                        className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
+                        placeholder="Kata Sandi"/>
+                        {errors.password && <p className="px-2 pt-1 text-xs font-medium text-red-500">{errors.password.message}</p>}
+                    </div>
+                    <div className="block text-sm mt-5">
+                      <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Jenis Kelamin</label>
+                      <select {...register('jenis_kelamin', {
+                        required:{
+                          value:true,
+                          message:'Pilih jenis kelamin!'
+                        }
+                      })} 
+                      className="form-select mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm">
+                        <option value='Pilih Jenis Kelamin'>Pilih Jenis Kelamin</option>
+                        <option value='Laki-laki'>Laki-laki</option>
+                        <option value='Perempuan'>Perempuan</option>
+                      </select>
+                      {errors.no_hp && <p className="px-2 pt-1 text-xs font-medium text-red-500">{errors.no_hp.message}</p>}
+                    </div>
                   </div>
                 </div>
+                <div className="block text-sm mt-5 2lg:mt-0">
+                  <label className="text-gray-800 font-medium"><span className="text-red-500">&nbsp;*</span>Alamat Usaha</label>
+                    <textarea {...register('alamat', {
+                      required:{
+                        value:true,
+                        message:'Masukkan alamat!'
+                      }
+                    })}
+                      className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm"
+                      placeholder="Jl. Raya Mauk No.89, Sepatan, Tangerang, Banten 15520, Indonesia"></textarea>
+                      {errors.alamat && <p className="px-2 pt-1 text-xs font-medium text-red-500">{errors.alamat.message}</p>}
+                    </div>          
                 <div className="text-center">
-                  <button onClick={handleSubmit} type="submit" className="w-52 h-8 mt-6 text-xs rounded text-slate-50 bg-fuchsia-600 hover:bg-fuchsia-500 shadow hover:shadow-fuchsia-500/50">
-                    Masuk
+                  <button type="submit" className="w-52 h-8 mt-6 text-xs rounded text-slate-50 bg-fuchsia-600 hover:bg-fuchsia-500 shadow hover:shadow-fuchsia-500/50">
+                    Daftar
                   </button>
                 </div>
+                
+                <div>
+                  <p className="text-center text-xs text-gray-800 mt-6">
+                    Sudah punya akun?
+                    <Link href="/login">
+                      <a className="font-semibold text-fuchsia-600"> Masuk Disini</a>
+                    </Link>
+                  </p>
+                </div>
               </form>
-              
-              <div>
-                <p className="text-center text-xs text-gray-800 mt-6">
-                  Sudah punya akun?
-                  <Link href="/login">
-                    <a className="font-semibold text-fuchsia-600"> Masuk Disini</a>
-                  </Link>
-                </p>
-              </div>
             </div>
           </div>
         </section>
