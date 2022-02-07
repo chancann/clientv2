c9f8c602cb21ae9c9425c4e09b8112bce5216afe
Chandra Riyadi <chandra.riyadi@raharja.info>
2022-02-07 21:03:46 +0700

    API

--- api/baseURL.js
+++ api/baseURL.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const baseURL = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://admin-client-server.herokuapp.com'
 });
 
 export default baseURL;
