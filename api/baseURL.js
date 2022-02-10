47cded336fd64bbaeb3c56ef20f0cbd101abbf9b
Giaz Pawaz <78998804+Giaz18@users.noreply.github.com>
2022-02-10 22:09:11 +0700

    SELESAI KAH

--- api/baseURL.js
+++ api/baseURL.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const baseURL = axios.create({
-  // baseURL: 'https://admin-client-server.herokuapp.com'
-  baseURL: "http://localhost:8080",
+  baseURL: "https://admin-client-server.herokuapp.com",
+  // baseURL: "http://localhost:8080",
 });
 
 export default baseURL;
--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -1,39 +1,72 @@
 import Link from "next/link";
 import MainLayout from "../components/layouts/MainLayout";
+import baseUrl from "../api/baseURL";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function forgotPassword() {
+  const [email, setEmail] = useState("");
+
+  const forgetPassword = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await baseUrl.put("/api/user/forget", { email });
+      if (response.data.status === 200) {
+        toast.success(response.data.data, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      } else {
+        toast.error(response.data.data, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
       <MainLayout>
         <section className="flex m-auto py-10 2lg:py-0 min-h-screen font-poppins">
-          <div className='flex w-full 2lg:mt-20'>
-            <div className='m-auto bg-gray-50/80 p-6 rounded-md'>
+          <div className="flex w-full 2lg:mt-20">
+            <div className="m-auto bg-gray-50/80 p-6 rounded-md">
               <div>
-                <h2 className='text-center text-sm font-bold tracking-wide text-gray-800'>
+                <h2 className="text-center text-sm font-bold tracking-wide text-gray-800">
                   Selamat Datang di Website
-                  <span className='text-lg font-extrabold tracking-wider text-fuchsia-600'> POJOK UMKM. </span>
+                  <span className="text-lg font-extrabold tracking-wider text-fuchsia-600"> POJOK UMKM. </span>
                 </h2>
               </div>
 
-              <form>
+              <form onSubmit={forgetPassword}>
                 <div className="py-6">
                   <div className="block text-sm">
                     <label className="px-2 text-gray-800 font-medium">Email</label>
-                    <input type="email" className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm" 
-                    placeholder="pojokumkm@contoh.com" />
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="form-input mt-1 block w-full text-sm rounded-md border-gray-300 shadow-sm" placeholder="pojokumkm@contoh.com" />
                   </div>
 
                   {/* Button */}
-                  <div className='text-center'>
-                    <Link href="/change-password">
-                      <button type='submit' className="w-52 h-8 mt-8 text-xs rounded text-slate-50 bg-fuchsia-600 hover:bg-fuchsia-500 shadow hover:shadow-fuchsia-500/50">
-                        Kirim Verifikasi
-                      </button>
-                    </Link>
+                  <div className="text-center">
+                    <button type="submit" className="w-52 h-8 mt-8 text-xs rounded text-slate-50 bg-fuchsia-600 hover:bg-fuchsia-500 shadow hover:shadow-fuchsia-500/50">
+                      Kirim Verifikasi
+                    </button>
                   </div>
 
                   <div>
-                    <p className='text-center text-xs text-gray-800 mt-6'>
+                    <p className="text-center text-xs text-gray-800 mt-6">
                       Belum punya akun?
                       <Link href="/register">
                         <a className="font-semibold text-fuchsia-600"> Daftar Disini</a>
@@ -47,5 +80,5 @@ export default function forgotPassword() {
         </section>
       </MainLayout>
     </>
-  )
+  );
 }
