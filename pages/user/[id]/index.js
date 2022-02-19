beff096b423c846904e8e3d6e9166c78a5529452
Chandra Riyadi <chandra.riyadi@raharja.info>
2022-02-19 13:54:00 +0700

    'PUSH'

--- pages/user/[id]/index.js
+++ pages/user/[id]/index.js
@@ -182,9 +182,9 @@ export default function profile() {
       />
       <MainLayout>
         <section className="flex items-center min-h-screen font-poppins">
-          <div className="w-full mt-20 2lg:mt-0">
-            <div className="w-full flex flex-col 2lg:flex-row items-center">
-              <figure className="md:w-6/12 text-center 2lg:mb-10">
+          <div className="w-full mt-20">
+            <div className="w-full flex flex-col items-center">
+              <figure className="md:w-6/12 text-center">
                 <div className="block text-center">
                   <h2 className="p-4 text-gray-800 font-bold text-3xl">
                     Profil
@@ -200,7 +200,7 @@ export default function profile() {
                 {/* <div>
                   <input onChange={onFileChange} type="file" className="mt-1 w-full text-gray-800 text-sm" />
                 </div> */}
-                <div className="flex items-center justify-center mt-2">
+                <div className="flex items-center justify-center mt-2 mb-4">
                   <label className="flex py-1 px-2 flex-col items-center rounded tracking-wide border cursor-pointer hover:bg-fuchsia-500 hover:text-white">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -227,19 +227,45 @@ export default function profile() {
               <div className="md:w-6/12 text-gray-800">
                 <div className="mt-10 2lg:mt-0">
                   <div className="w-full">
-                    <div className="relative text-sm">
-                      <label className="absolute px-2 text-gray-800 font-medium"></label>
+                    <div className="relative mb-4 text-sm">
+                      <label className="absolute px-2 left-0 inset-y-0 flex items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </label>
                       <input
                         type="text"
-                        className="form-input mt-1 block w-full text-sm border-none rounded-md shadow"
+                        className="form-input appearance-none pl-9 mt-1 block w-full text-sm border-none rounded-md shadow"
                         defaultValue={detailUser.nik}
                       />
                     </div>
-                    <div className="relative text-sm">
-                      <label className="absolute px-2 text-gray-800 font-medium"></label>
+                    <div className="relative mb-4 text-sm">
+                      <label className="absolute px-2 left-0 inset-y-0 flex items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </label>
                       <input
                         type="text"
-                        className="form-input mt-1 block w-full text-sm border-none rounded-md shadow"
+                        className="form-input appearance-none pl-9 mt-1 block w-full text-sm border-none rounded-md shadow"
                         onChange={(e) => {
                           setDetailUser({
                             ...detailUser,
@@ -249,11 +275,24 @@ export default function profile() {
                         defaultValue={detailUser.nama_lengkap}
                       />
                     </div>
-                    <div className="relative text-sm">
-                      <label className="absolute px-2 text-gray-800 font-medium"></label>
+                    <div className="relative mb-4 text-sm">
+                      <label className="absolute px-2 left-0 inset-y-0 flex items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V4a2 2 0 00-2-2H6zm4 14a1 1 0 100-2 1 1 0 000 2z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </label>
                       <input
                         type="text"
-                        className="form-input mt-1 block w-full text-sm border-none rounded-md shadow"
+                        className="form-input appearance-none pl-9 mt-1 block w-full text-sm border-none rounded-md shadow"
                         onChange={(e) => {
                           setDetailUser({
                             ...detailUser,
@@ -263,16 +302,39 @@ export default function profile() {
                         defaultValue={detailUser.no_hp}
                       />
                     </div>
-                    <div className="relative text-sm">
-                      <label className="absolute px-2 text-gray-800 font-medium"></label>
+                    <div className="relative mb-4 text-sm">
+                      <label className="absolute px-2 left-0 inset-y-0 flex items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                          <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                        </svg>
+                      </label>
                       <input
                         type="email"
-                        className="form-input mt-1 block w-full text-sm border-none rounded-md shadow"
+                        className="form-input appearance-none pl-9 mt-1 block w-full text-sm border-none rounded-md shadow"
                         defaultValue={detailUser.email}
                       />
                     </div>
-                    <div className="relative text-sm">
-                      <label className="absolute px-2 text-gray-800 font-medium"></label>
+                    <div className="relative mb-4 text-sm">
+                      <label className="absolute px-2 left-0 top-[6px] flex items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </label>
                       <textarea
                         onChange={(e) => {
                           setDetailUser({
@@ -281,13 +343,26 @@ export default function profile() {
                           });
                         }}
                         defaultValue={detailUser.alamat}
-                        className="form-input mt-1 block w-full text-sm border-none rounded-md shadow"
+                        className="form-input appearance-none pl-9 mt-1 block w-full text-sm border-none rounded-md shadow"
                       />
                     </div>
-                    <div className="relative text-sm">
-                      <label className="px-2 text-gray-800 absolute font-medium"></label>
+                    <div className="relative mb-4 text-sm">
+                      <label className="absolute px-2 left-0 inset-y-0 flex items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </label>
                       <input
-                        className="form-input mt-1 block w-full text-sm border-none rounded-md shadow"
+                        className="form-input appearance-none pl-9 mt-1 block w-full text-sm border-none rounded-md shadow"
                         type={isPasswordShow ? "text" : "password"}
                         onChange={(e) => {
                           setDetailUser({
@@ -297,36 +372,37 @@ export default function profile() {
                         }}
                         defaultValue={detailUser.password}
                       />
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        onClick={() => {
-                          setIsPasswordShow(!isPasswordShow);
-                        }}
-                        className="absolute h-4 w-4 cursor-pointer inset-y-0 right-0 flex items-center text-sm leading-5 mt-[10px] mr-3"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          className={!isPasswordShow ? "block" : "hidden"}
-                          fillRule="evenodd"
-                          d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
-                          clipRule="evenodd"
-                        />
-                        <path
-                          className={!isPasswordShow ? "block" : "hidden"}
-                          d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"
-                        />
-                        <path
-                          className={isPasswordShow ? "block" : "hidden"}
-                          d="M10 12a2 2 0 100-4 2 2 0 000 4z"
-                        />
-                        <path
-                          className={isPasswordShow ? "block" : "hidden"}
-                          fillRule="evenodd"
-                          d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
+                      <div className="absolute h-4 w-4 cursor-pointer inset-y-0 right-0 flex items-center mt-[10px] mr-3">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          onClick={() => {
+                            setIsPasswordShow(!isPasswordShow);
+                          }}
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            className={!isPasswordShow ? "block" : "hidden"}
+                            fillRule="evenodd"
+                            d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
+                            clipRule="evenodd"
+                          />
+                          <path
+                            className={!isPasswordShow ? "block" : "hidden"}
+                            d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z"
+                          />
+                          <path
+                            className={isPasswordShow ? "block" : "hidden"}
+                            d="M10 12a2 2 0 100-4 2 2 0 000 4z"
+                          />
+                          <path
+                            className={isPasswordShow ? "block" : "hidden"}
+                            fillRule="evenodd"
+                            d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -349,7 +425,7 @@ export default function profile() {
           </div>
         </section>
 
-        <section className="font-poppins">
+        <section className="font-poppins mt-6">
           <div className="text-gray-800">
             <div className="text-center">
               <h2 className="p-4 font-bold text-3xl">Produk Anda</h2>
