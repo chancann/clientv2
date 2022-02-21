54bac6a933a1b82240d44aa7c16a543f80dfb6a5
Chandra Riyadi <chandra.riyadi@raharja.info>
2022-02-21 23:25:49 +0700

    'Finishing'

--- pages/product/index.js
+++ pages/product/index.js
@@ -84,27 +84,47 @@ export default function product() {
             <div className="text-center">
               <h2 className="p-4 font-bold text-3xl">Produk</h2>
             </div>
+            {/* <div className="flex justify-center items-center">
+              <div
+                className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+                role="status"
+              >
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </div> */}
             <div className="flex mb-6">
               <div className="w-full relative">
-                <select
-                  value={category}
-                  onChange={(e) => {
-                    setCategory(e.target.value);
-                  }}
-                  name="cars"
-                  id="cars"
-                >
-                  <option disabled selected="selected">
-                    Category
-                  </option>
-                  <option value="all">Semua Category</option>
-                  <option value="Makanan">Makanan</option>
-                  <option value="Minuman">Minuman</option>
-                  <option value="Pakaian">Pakaian</option>
-                  <option value="Kerajinan Tangan">Kerajinan Tangan</option>
-                  <option value=" Perawatan Tubuh"> Perawatan Tubuh</option>
-                </select>
                 <div className="flex justify-end">
+                  <select
+                    value={category}
+                    onChange={(e) => {
+                      setCategory(e.target.value);
+                    }}
+                    name="cars"
+                    id="cars"
+                    className="
+                  block
+                  cursor-pointer
+                  px-3
+                  py-1.5
+                  text-sm
+                  text-gray-700
+                  border-none
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:outline-none"
+                  >
+                    <option disabled selected="selected">
+                      Pilih Kategori
+                    </option>
+                    <option value="all">Semua Kategori</option>
+                    <option value="Makanan">Makanan</option>
+                    <option value="Minuman">Minuman</option>
+                    <option value="Pakaian">Pakaian</option>
+                    <option value="Kerajinan Tangan">Kerajinan Tangan</option>
+                    <option value="Perawatan Tubuh"> Perawatan Tubuh</option>
+                  </select>
+                </div>
+                {/* <div className="flex justify-end">
                   <div className="">
                     <button
                       onClick={() => {
@@ -117,38 +137,142 @@ export default function product() {
                       aria-haspopup="true"
                     >
                       Pilih Kategori
-                      <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                      <svg
+                        className="-mr-1 ml-2 h-5 w-5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
                       </svg>
                     </button>
                   </div>
-                </div>
+                </div> */}
 
-                <div className={`${isOpen ? "block" : "hidden"} z-10 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
+                {/* <div
+                  className={`${
+                    isOpen ? "block" : "hidden"
+                  } z-10 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`}
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="menu-button"
+                  tabIndex={-1}
+                >
                   <div className="py-1" role="none">
-                    <a href="/" className="block px-4 py-2 text-sm hover:bg-gray-50" role="menuitem" tabIndex={-1} id="menu-item-0">
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-sm hover:bg-gray-50"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="menu-item-0"
+                    >
                       Makanan
                     </a>
-                    <a href="/" className="block px-4 py-2 text-sm hover:bg-gray-50" role="menuitem" tabIndex={-1} id="menu-item-0">
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-sm hover:bg-gray-50"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="menu-item-0"
+                    >
                       Minuman
                     </a>
-                    <a href="/" className="block px-4 py-2 text-sm hover:bg-gray-50" role="menuitem" tabIndex={-1} id="menu-item-0">
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-sm hover:bg-gray-50"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="menu-item-0"
+                    >
                       Pakaian
                     </a>
-                    <a href="/" className="block px-4 py-2 text-sm hover:bg-gray-50" role="menuitem" tabIndex={-1} id="menu-item-0">
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-sm hover:bg-gray-50"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="menu-item-0"
+                    >
                       Kerajinan Tangan
                     </a>
-                    <a href="/" className="block px-4 py-2 text-sm hover:bg-gray-50" role="menuitem" tabIndex={-1} id="menu-item-0">
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-sm hover:bg-gray-50"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="menu-item-0"
+                    >
                       Perawatan Tubuh
                     </a>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
             <div className="grid grid-cols-1 2sm:grid-cols-2 md:grid-cols-3 2lg:grid-cols-4 gap-4">
               {products.map((prod) => (
-                <Card prodId={prod._id} key={prod._id} title={prod.title} createdAt={moment(prod.createdAt).locale("id")} price={prod.price} author={prod.author ? prod.author.nama_lengkap : "Anonimous"} img={`${baseURL.defaults.baseURL}/${prod.images[0].data}`} />
+                <Card
+                  prodId={prod._id}
+                  key={prod._id}
+                  title={prod.title}
+                  createdAt={moment(prod.createdAt).locale("id")}
+                  price={prod.price}
+                  author={prod.author ? prod.author.nama_lengkap : "Anonimous"}
+                  img={`${baseURL.defaults.baseURL}/${prod.images[0].data}`}
+                />
               ))}
+            </div>
+            <div className="flex justify-center mt-4">
+              <nav>
+                <ul className="flex list-style-none">
+                  <li className="page-item">
+                    <a
+                      className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 text-gray-800 hover:text-gray-800 focus:shadow-none"
+                      href="#"
+                      aria-label="Previous"
+                    >
+                      <span aria-hidden="true">&laquo;</span>
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a
+                      className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
+                      href="#"
+                    >
+                      1
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a
+                      className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
+                      href="#"
+                    >
+                      2
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a
+                      className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
+                      href="#"
+                    >
+                      3
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a
+                      className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
+                      href="#"
+                      aria-label="Next"
+                    >
+                      <span aria-hidden="true">&raquo;</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
             </div>
           </div>
         </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,36 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
     screens: {
-      'sm': '320px',
-      '2sm': '480px',
-      'md': '640px',
-      'lg': '768px',
-      '2lg': '860px',
-      'xl': '1024px',
-      '2xl': '1280px',
-      '3xl': '1440px',
-      '4xl': '1536px',
+      sm: "320px",
+      "2sm": "480px",
+      md: "640px",
+      lg: "768px",
+      "2lg": "860px",
+      xl: "1024px",
+      "2xl": "1280px",
+      "3xl": "1440px",
+      "4xl": "1536px",
     },
     fontFamily: {
-      'sans': ['ui-sans-serif', 'system-ui'],
-      'serif': ['ui-serif', 'Georgia'],
-      'mono': ['ui-monospace', 'SFMono-Regular'],
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
       // 'jost':['Jost', 'sans-serif'],
-      'poppins': ['Poppins', 'sans-serif'],
+      poppins: ["Poppins", "sans-serif"],
       // 'montserrat':['Montserrat', 'sans-serif'],
       // 'inter':['Inter', 'sans-serif'],
     },
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')({
-    strategy:'class',
-  }), ],
-}
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    }),
+    require("tw-elements/dist/plugin")
+  ],
+};
